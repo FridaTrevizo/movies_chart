765313e400f1dd1fc6eac37df6eb7ce4e61932d4
FridaTrevizo <33066660+FridaTrevizo@users.noreply.github.com>
2020-06-13 00:02:16 -0500

    dashboards

--- parsi_thigy.js
+++ parsi_thigy.js
@@ -1,9 +1,15 @@
 makeItChart1();
 makeItChart2();
+makeItChart3();
+makeItChart4();
 const x_labels = [];
 const y_data = [];
 const x_labels_2 = [];
 const y_data_2 = [];
+const x_labels_3 = [];
+const y_data_3 = [];
+const x_labels_4 = [];
+const y_data_4 = [];
 
 async function getData() {
     const movie_response = await fetch('states.csv');
@@ -16,10 +22,16 @@ async function getData() {
         const state = columns[0];
         const avg_month_pay = columns[8];
         const coverage = columns[4];
+        const employer = columns[5];
+        const tax = columns[8];
         x_labels.push(state);
         y_data.push(avg_month_pay);
         y_data_2.push(coverage);
         x_labels_2.push(avg_month_pay);
+        x_labels_3.push(state);
+        y_data_3.push(employer);
+        x_labels_4.push(state);
+        y_data_4.push(tax);
         /*const popularity = columns[10];
         const premier = columns[14];
         const revenue = columns[15];
@@ -73,7 +85,7 @@ async function makeItChart2() {
     const ctx = document.getElementById('chart_2').getContext('2d');
 
     const myChart = new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
             labels: x_labels_2,
             datasets: [{
@@ -104,3 +116,74 @@ async function makeItChart2() {
     });
 }
 
+async function makeItChart3() {
+    await getData();
+    const ctx = document.getElementById('chart_3').getContext('2d');
+
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: x_labels_3,
+            datasets: [{
+                label: 'Avarage employer inssurance',
+                data: y_data_3,
+                backgroundColor:
+                    'rgba(19, 57, 159, 0.2)'
+                ,
+                borderColor:
+                    'rgba(19, 99, 132, 1)'
+                ,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            tooltips: {
+
+            }
+
+        }
+    });
+}
+
+async function makeItChart4() {
+    await getData();
+    const ctx = document.getElementById('chart_4').getContext('2d');
+
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: x_labels_4,
+            datasets: [{
+                label: 'Avarage marketplace tax per state',
+                data: y_data_4,
+                backgroundColor:
+                    'rgba(19, 57, 159, 0.2)'
+                ,
+                borderColor:
+                    'rgba(19, 99, 132, 1)'
+                ,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            tooltips: {
+
+            }
+
+        }
+    });
+}
